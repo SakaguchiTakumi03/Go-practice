cb109d3d1072d9426efa2917243a746a45ca6db1
SakaguchiTakumi03 <sakaguchi.takumi03@gmail.com>
2022-11-12 17:23:04 +0900

    レシーバとmathを用いた計算

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"strconv"
 	"time"
@@ -25,17 +26,43 @@ func main() {
 	// }
 	// fmt.Println("ループ数：" + strconv.Itoa(count) + "回")
 
-	var u User
-	u.age = 20
-	u.favorite = "読書"
-	u.gender = "男"
-	fmt.Println(u)
+	// u.age = 20
+	// u.favorite = "読書"
+	// u.gender = "男"
+	// u := User{"男", 20, "読書"} //上のコメントでもこっちでもどちらでも大丈夫
+	// u := User{20, "男", "読書"} structの構造に合わせた順番にしないといけない
+	// fmt.Println(u)
+	// result := Student{"hogehoge", 55, 60}
+	// s := Student{"hogehoge", 60, 60}
+	// avg_result(s)
+	BMI_result(43, 163)
 }
+
+type Student struct {
+	name                 string
+	math_point, eg_point float64
+}
+
+// type BMI_Point struct {
+// 	weight, height float64
+// }
 
 type User struct {
 	gender   string
 	age      int
 	favorite string
+}
+
+func BMI_result(weight float64, height float64) {
+	result_height := height / 100.0
+	var result = weight / math.Pow(result_height, 2)
+	fmt.Println("身長：", height)
+	fmt.Println("体重：", weight)
+	fmt.Println("BMIの結果は", math.Floor(result*100)/100, "です")
+}
+
+func avg_result(s Student) {
+	fmt.Println(s.name, "君の点数は", (s.eg_point+s.math_point)/2, "点です")
 }
 
 func message(age int) string {
