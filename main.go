c5822b55cfde34422f2184f3d861048196dea89d
SakaguchiTakumi03 <sakaguchi.takumi03@gmail.com>
2022-11-12 16:32:20 +0900

    struct を学んだ

--- main.go
+++ main.go
@@ -1,36 +1,48 @@
 package main
 
-import(
+import (
 	"fmt"
-	"time"
-	"strconv"
 	"math/rand"
+	"strconv"
+	"time"
 )
 
-func main()  {
+func main() {
 	// fmt.Println("hi~")
-	a := [3]string{"hoge","fuga","hogehoge"}
+	a := [3]string{"hoge", "fuga", "hogehoge"}
 	_ = a
 	// a := [...]string{"hoge","fuga","hogehoge"}
 	// a[2] = "fugafuga"
 	// b := [...][...]string{{"hoge","hoge","hoge"},{"fuga","fuga","fuga"},{"pui","pui","pui"}}
 
 	rand.Seed(time.Now().UnixNano())
-	count := rand.Intn(20)
+	// count := rand.Intn(20)
 
-	for i := 0 ; i < count ; i++{
-		age := rand.Intn(30)
-		result := message(age)
-		fmt.Println(result)
-	}
-	fmt.Println("ループ数："+strconv.Itoa(count)+"回")
+	// for i := 0; i < count; i++ {
+	// 	age := rand.Intn(30)
+	// 	result := message(age)
+	// 	fmt.Println(result)
+	// }
+	// fmt.Println("ループ数：" + strconv.Itoa(count) + "回")
+
+	var u User
+	u.age = 20
+	u.favorite = "読書"
+	u.gender = "男"
+	fmt.Println(u)
+}
+
+type User struct {
+	gender   string
+	age      int
+	favorite string
 }
 
 func message(age int) string {
 	str_age := strconv.Itoa(age)
-	if age >= 20{
-		return str_age+"歳なんだ、もう大人だね！"
-	}else{
-		return str_age+"歳なんだ、大人じゃないのだよ。。。"
+	if age >= 20 {
+		return str_age + "歳なんだ、もう大人だね！"
+	} else {
+		return str_age + "歳なんだ、大人じゃないのだよ。。。"
 	}
 }
